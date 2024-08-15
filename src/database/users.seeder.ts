1722ce98ff930e535eae0e157873819af0a775dc
makloster <klostermanuelandres@gmail.com>
2024-08-15 19:46:40 -0300

    user seeder refactoring

--- src/database/users.seeder.ts
+++
@@ -1,9 +0,0 @@
-import { DataSource } from "typeorm"
-import { dsOptions } from "./data-source"
-import { runSeeders } from "typeorm-extension"
-
-export const dataSource = new DataSource(dsOptions)
-dataSource.initialize().then(async () => {
-  await runSeeders(dataSource)
-  process.exit()
-})
